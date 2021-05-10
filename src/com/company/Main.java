86a74007db205b7b3f4aa1ea90877ddd4a645d06
Điền Trần Vĩnh An <dienvinhan@gmail.com>
2021-05-10 09:49:54 +0700

    Function 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.function.DoubleToIntFunction;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("function 1");
+
+}
